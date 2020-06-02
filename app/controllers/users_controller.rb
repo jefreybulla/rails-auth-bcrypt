2dfa9a520bb0fec65957f68d4fa9a8386f38a707
jefreybulla <jefrey.bulla@gmail.com>
2020-06-01 23:27:44 -0400

    validate email and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,14 @@ class UsersController < ApplicationController
     puts params[:user]
     puts "here!!!"
     @user = User.new(params.require(:user).permit(:email, :password))
-    if @user.save
+
+    begin
+      @user.save!
       session[:user_id] = @user.id
       redirect_to root_url, notice: 'Thank you for signing up!'
-    else
+    rescue => e
+      puts "####################"
+      puts " Error #{e.message}"
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'bcrypt'
 
 class User < ApplicationRecord
   include BCrypt
-  
+  validates :email, :password, presence: true
   validates_uniqueness_of :email
 
   # method to verify password
@@ -12,8 +12,10 @@ class User < ApplicationRecord
 
   # method to create a new password
   def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_digest = @password
+    if new_password != ''
+      @password = Password.create(new_password)
+      self.password_digest = @password
+    end
   end
   
 end
