c02997c4f3d7d5d77d65d2cfba8e5c49f0db686e
jefreybulla <jefrey.bulla@gmail.com>
2020-06-08 19:27:48 -0400

    change secure

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None', secure: true
+#Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None', secure: true
+
+# Production: add secure: true
+Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None'
+
+# To make it work in Chrome Incognito. Cookie is set in s1.ngrok.io and used in subdomain.s1.ngrok.io. 
+# Edit config/initializers/session_store.rb accordingly. 
+# Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: ‘<Lax or Strict>', domain: ‘s1.ngrok.io', secure: true
