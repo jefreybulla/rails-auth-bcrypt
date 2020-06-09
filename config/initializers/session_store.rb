edda3535bc5312602cab95c0fbf0aaba6914e9aa
jefreybulla <jefrey.bulla@gmail.com>
2020-06-08 20:35:55 -0400

    add comments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
+# The following works fine in most browsers with the exception of private mode on Chrome and Safari as of 6/8/2020
+# It works even if domains are different: set cookie on example.com and use cookie on anotherexample.com
 #Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None', secure: true
 
-# Production: add secure: true
-Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'Strict', domain: 'rails.toro-labs.com'
-
-# To make it work in Chrome Incognito. Cookie is set in s1.ngrok.io and used in subdomain.s1.ngrok.io. 
+# The following works fine in most browsers with the exception of private mode on Safari as of 6/8/2020
+# Cookie is set in rails.toro-labs.com and used in subdomain.rails.toro-labs.com
+# Production: add secure: true. Require that both websites use SSL. 
 # Edit config/initializers/session_store.rb accordingly. 
-# Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: ‘<Lax or Strict>', domain: ‘s1.ngrok.io', secure: true
+Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'Strict', domain: 'rails.toro-labs.com'
