bcb0440d40e3ac39dc83d09e1fe91a965ed6d352
jefreybulla <jefrey.bulla@gmail.com>
2020-06-08 20:04:53 -0400

    edit same-site to lax and add domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,7 @@
 #Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None', secure: true
 
 # Production: add secure: true
-Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'None'
+Rails.application.config.session_store :cookie_store, key: '_rails-auth_session', same_site: 'Lax', domain: 'rails.toro-labs.com'
 
 # To make it work in Chrome Incognito. Cookie is set in s1.ngrok.io and used in subdomain.s1.ngrok.io. 
 # Edit config/initializers/session_store.rb accordingly. 
